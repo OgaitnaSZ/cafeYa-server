306b11de979cb898a12cca242b584a92741f079b
OgaitnaSZ <zanty201212@gmail.com>
2026-01-25 13:08:07 -0300

    Controller para productos

--- src/controllers/producto.ts
+++ src/controllers/producto.ts
@@ -6,10 +6,52 @@ const prisma = new PrismaClient()
 
 // Obtener Producto
 export async function obtenerProducto(req: Request, res: Response) {
-  
+      try {
+        const data = matchedData(req);
+    
+        const existingProduct = await prisma.producto.findUnique({
+            where: { id: data.id }
+        });
+          
+        if(!existingProduct) return handleHttpError(res, "PRODUCTO NO EXISTE", 404)
+
+        return res.status(200).json(existingProduct);
+    } catch (err) {
+      handleHttpError(res, "No se pudo actualizar el cliente", 500)
+      return;
+    }
 }
 
 // Obtener Productos
 export async function obtenerProductos(req: Request, res: Response) {
-  
+    try {
+        const data = matchedData(req);
+        const { categoria, search } = data;
+
+        const existingProducts = await prisma.producto.findMany({
+            where: {
+                AND: [
+                    { disponibilidad: true },
+                    categoria ? { categoria } : {},
+                    search
+                        ? {
+                            OR: [
+                                { categoria: { contains: search } },
+                                { nombre: { contains: search } },
+                                { descripcion: { contains: search } }
+                            ]
+                        }
+                        : {}
+                ]
+            }
+        });
+
+        if (existingProducts.length === 0) {
+            return handleHttpError(res, "No hay productos coincidentes", 404);
+        }
+
+        return res.status(200).json(existingProducts);
+    } catch (err) {
+        return handleHttpError(res, "Error al obtener productos", 500);
+    }
 }
--- src/routes/producto.ts
+++ src/routes/producto.ts
@@ -2,12 +2,11 @@ import express from "express";
 const router = express.Router();
 import * as producto from "../controllers/producto";
 import * as validator from "../validators/producto";
-import { authMiddleware } from "../middleware/session";
 
 // Obtener producto
-router.get("/productos", authMiddleware, validator.validatorProductos, producto.obtenerProductos);
+router.get("/productos", validator.validatorProductos, producto.obtenerProductos);
 
 // Agregar productos
-router.get("/producto/:id", authMiddleware, validator.validatorId, producto.obtenerProducto);
+router.get("/:id", validator.validatorId, producto.obtenerProducto);
 
 export { router };
