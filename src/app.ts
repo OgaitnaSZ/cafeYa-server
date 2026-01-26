1639bc8529166ce87a38cbc1b43ca81936d62081
OgaitnaSZ <zanty201212@gmail.com>
2026-01-26 20:06:16 -0300

    Configurar como ruta estatica /uploads para las fotos

--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,12 @@ import cors from "cors";
 import dotenv from 'dotenv';
 dotenv.config();
 import routes from "./routes/index";
+const path = require('path');
 
 const app = express();
 
 app.use(express.json());
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(cors());
 
 // Importacion dinamica de rutas
